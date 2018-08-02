85480c89e2c8e9c438975dbf87da153224552a83
Edward Zhang <edward64@gmail.com>
2018-08-01 23:19:25 -0700

    Update/notifications even when app not running

--- androidapp/app/src/main/java/com/ilogues/ed/processtracker/Main2Activity.java
+++ androidapp/app/src/main/java/com/ilogues/ed/processtracker/Main2Activity.java
@@ -4,6 +4,10 @@ import android.app.FragmentManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -26,7 +30,7 @@ public class Main2Activity extends AppCompatActivity {
     JobsListRequestTask requestTask;
     Handler handler;
 
-    static final String CHANNEL_ID = "ProcessTrackerNotifications";
+    static final int JOB_ID = 7428;
     List<ProcessViewFragment> frags;
 
     @Override
@@ -57,7 +61,14 @@ public class Main2Activity extends AppCompatActivity {
                 handler.postDelayed(this, delay_s*1000);
             }
         }, 1000);
-        createNotificationChannel();
+        JobScheduler jobScheduler =
+                (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(new JobInfo.Builder(JOB_ID,
+                new ComponentName(this, UpdateService.class))
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPeriodic(JobInfo.getMinPeriodMillis(), JobInfo.getMinFlexMillis())
+                .setPersisted(true)
+                .build());
     }
 
     // Menu icons are inflated just as they were with actionbar
@@ -66,65 +77,6 @@ public class Main2Activity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.actionbarmenu, menu);
         return true;
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-    private class NotificationTask extends AsyncTask<Void, Void, Void>
-    {
-        String jobid;
-        String jobName;
-
-        public NotificationTask(String jobName, String started) {
-            this.jobid = jobName + "_" + started;
-            this.jobName = jobName;
-        }
-
-        @Override
-        protected Void doInBackground (Void... voids){
-            JobNotificationDB db = JobNotificationDB.getDatabase(Main2Activity.this);
-            JobNotificationRecord rec = db.jnDao().findRecord(jobid);
-            if (rec == null) {
-                Log.i("Main2Activity", "NotifyCompletion: notify " + jobid);
-                // Create an explicit intent for an Activity in your app
-                Intent intent = new Intent(Main2Activity.this, Main2Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(Main2Activity.this, 0, intent, 0);
-                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(Main2Activity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_completed)
-                        .setContentTitle("Process Completed")
-                        .setContentText("Process '" + jobName + "' completed.")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true);;
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(Main2Activity.this);
-                rec = new JobNotificationRecord(jobid);
-                notificationManager.notify(rec.hashCode(), mBuilder.build());
-                db.jnDao().insert(rec);
-            }
-            return null;
-        }
-    }
-    private void NotifyCompletion(String jobName, String started) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean notificationsenabled = prefs.getBoolean("notifications", false);
-        if (notificationsenabled) {
-            NotificationTask t = new NotificationTask(jobName, started);
-            t.execute();
-        }
     }
 
     public void onRefreshClick(MenuItem mi) {
@@ -138,9 +90,6 @@ public class Main2Activity extends AppCompatActivity {
             public void processResponse(JobsList l) {
                 // TODO: Sort by updated? (or do that in request)
                 // TODO: Keep the same fragment for the same job for display settings
-                for (JobsList.Job job : l.jobs) {
-                    if (job.completed) NotifyCompletion(job.jobName, job.started);
-                }
                 for (int i = 0; i < frags.size(); i++) {
                     ProcessViewFragment v = frags.get(i);
                     if (i < l.jobs.size()) {
---
+++ androidapp/app/src/main/java/com/ilogues/ed/processtracker/UpdateService.java
@@ -0,0 +1,117 @@
+package com.ilogues.ed.processtracker;
+
+import android.app.FragmentManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.arch.persistence.room.Update;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+public class UpdateService extends JobService {
+    static final String CHANNEL_ID = "ProcessTrackerNotifications";
+
+    JobsListRequestTask requestTask;
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    private class NotificationTask extends AsyncTask<Void, Void, Void>
+    {
+        String jobid;
+        String jobName;
+
+        public NotificationTask(String jobName, String started) {
+            this.jobid = jobName + "_" + started;
+            this.jobName = jobName;
+        }
+
+        @Override
+        protected Void doInBackground (Void... voids){
+            JobNotificationDB db = JobNotificationDB.getDatabase(UpdateService.this);
+            JobNotificationRecord rec = db.jnDao().findRecord(jobid);
+            if (rec == null) {
+                Log.i("UpdateService", "NotifyCompletion: notify " + jobid);
+                // Create an explicit intent for an Activity in your app
+                Intent intent = new Intent(UpdateService.this, Main2Activity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(UpdateService.this, 0, intent, 0);
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(UpdateService.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_completed)
+                        .setContentTitle("Process Completed")
+                        .setContentText("Process '" + jobName + "' completed.")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);;
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(UpdateService.this);
+                rec = new JobNotificationRecord(jobid);
+                notificationManager.notify(rec.hashCode(), mBuilder.build());
+                db.jnDao().insert(rec);
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final String sheeturl = prefs.getString("sheets_url", "");
+        final String apikey = getString(R.string.gapikey);
+        Log.i("UpdateService", "Started job");
+        if (sheeturl.isEmpty()) {
+            jobFinished(params, false);
+            return false;
+        }
+        Log.i("UpdateService", "Checking for completion");
+        requestTask = new JobsListRequestTask(apikey, sheeturl, new JobsListRequestTask.JobsListRequestCallback() {
+            @Override
+            public void processResponse(JobsList l) {
+                createNotificationChannel();
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(UpdateService.this);
+                boolean notificationsenabled = prefs.getBoolean("notifications", false);
+                if (notificationsenabled) {
+                    for (JobsList.Job job : l.jobs) {
+                        if (job.completed) {
+                            NotificationTask t = new NotificationTask(job.jobName, job.started);
+                            t.execute();
+                        }
+                    }
+                }
+                jobFinished(params, false);
+            }
+        });
+        requestTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        requestTask.cancel(true);
+        return true;
+    }
+}
